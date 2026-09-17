085e17fd5a860002465dd75640e83a1b5f1da4ee
3tarSeed <adam.gouda@gmail.com>
2026-09-16 22:06:30 -0400

    Delete m directory

--- m/app/leads/page.tsx
+++
@@ -1,7 +0,0 @@
-import { requireStaffUser } from "@/app/auth";
-import Desk from "./desk";
-export const dynamic = "force-dynamic";
-export default async function Page() {
-  await requireStaffUser("/leads");
-  return <Desk />;
-}
